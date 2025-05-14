6648d770ad04cc4c6f9ea0e588fdb9b12638a7a8
tishasheth04 <shethtisha@gmail.com>
2025-05-13 21:01:25 -0400

    Testing

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Login = () => {
     return (
         <h1 className="text-3xl font-bold underline">
-            Hello world!
+            Hello world!!
         </h1>
     )
     
