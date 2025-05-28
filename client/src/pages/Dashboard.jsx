8c328f673fdcee9cd55e8f3563c792e42dc3096c
tishasheth04 <shethtisha@gmail.com>
2025-05-28 18:33:05 -0400

    making some progress on the actual dashboard aspect

--- client/src/pages/Dashboard.jsx
+++ client/src/pages/Dashboard.jsx
@@ -1,8 +1,83 @@
 import React from 'react'
 
+import{
+    MdAdminPanelSettings,
+    MdKeyboardArrowDown,
+    MdKeyboardArrowUp,
+    MdKeyboardDoubleArrowUp,
+} from "react-icons/md";
+import { LuClipboardEdit } from "react-icons/lu";
+import { FaNewspaper, FaUsers } from "react-icons/fa";
+import { FaArrowsToDot } from "react-icons/fa6";
+import moment from "moment";
+
+/*import { summary } from "../assets/data";
+import clsx from "clsx";
+import { Chart } from "../components/Chart";
+import { BGS, PRIOTITYSTYELS, TASK_TYPE, getInitials } from "../utils";
+import UserInfo from "../components/UserInfo";*/
+
 const Dashboard = () => {
-    return <div>Dashboard</div>;
-    
+
+    const totals = summary.tasks
+    const stats = [
+    {
+      _id: "1",
+      label: "TOTAL TASK",
+      total: summary?.totalTasks || 0,
+      icon: <FaNewspaper />,
+      bg: "bg-[#1d4ed8]",
+    },
+    {
+      _id: "2",
+      label: "COMPLTED TASK",
+      total: totals["completed"] || 0,
+      icon: <MdAdminPanelSettings />,
+      bg: "bg-[#0f766e]",
+    },
+    {
+      _id: "3",
+      label: "TASK IN PROGRESS ",
+      total: totals["in progress"] || 0,
+      icon: <LuClipboardEdit />,
+      bg: "bg-[#f59e0b]",
+    },
+    {
+      _id: "4",
+      label: "TODOS",
+      total: totals["todo"],
+      icon: <FaArrowsToDot />,
+      bg: "bg-[#be185d]" || 0,
+    },
+  ];
+
+    const Card = ({label, count, bg, icon})=>{
+        return(
+            <div className='w-full h-32 bg-white p-5 shadow-md rounded-md flex items-center justify-between'>
+                <div className='h-full flex flex-1 flex-col justify-between'>
+                    <p>{label}</p>
+                </div>
+            </div>
+        );
+    };
+    return (
+        <div className='h-full py-4'>
+            <div className='grid grid-cols-1 md:grid-cols-4 gap-5'>
+            {
+                stats.map(({icon, bg, label, total}, index)=> (
+                    <Card
+                    key={index}
+                    icon={icon}
+                    bg={bg}
+                    label={label}
+                    count={total}
+
+                    />
+                ))
+            }
+        </div>
+    </div>
+    );
 };
 
 export default Dashboard;
